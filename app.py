b4a4dd9ae40b4f346175b8134e3d04209fa14a02
Krishna <kkrishn@searshc.com>
2024-06-12 14:44:22 +0530

    changed asset urls

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ featured_jobs = [
         "minSalary": 11,
         "maxSalary": 17,
         "tags": ["JavaScript", "React", "CSS"],
-        "iconUrl": "openAILogo",
+        "iconUrl": "./src/assets/openAILogo",
         "posted": "16-May-2023",
     },
     {
@@ -29,7 +29,7 @@ featured_jobs = [
         "minSalary": 105,
         "maxSalary": 150,
         "tags": ["Node.js", "Express", "MongoDB"],
-        "iconUrl": "openAILogo",
+        "iconUrl": "./src/assets/openAILogo",
         "posted": "20-May-2023",
     },
     {
@@ -43,7 +43,7 @@ featured_jobs = [
         "minSalary": 6,
         "maxSalary": 9,
         "tags": ["JavaScript", "React", "Node.js"],
-        "iconUrl": "redStringLogo",
+        "iconUrl": "./src/assets/redStringLogo",
         "posted": "21-May-2023",
     },
     {
@@ -57,7 +57,7 @@ featured_jobs = [
         "minSalary": 10,
         "maxSalary": 25,
         "tags": ["Python", "Machine Learning", "SQL"],
-        "iconUrl": "amazonLogo",
+        "iconUrl": "./src/assets/amazonLogo",
         "posted": "21-May-2023",
     },
 ]
